ae92450479aaa0de1e963403351fbc1ba24cfab3
brausjonas <brausjonas@gmail.com>
2023-03-25 23:07:55 +0100

    Better

--- StartPage.js
+++ StartPage.js
@@ -4,11 +4,13 @@ function showMobileMenuBar()
 {
     if(!menuActive) {
         document.getElementById("mobileMenuBar").style.display = "block";
+        document.getElementById("mobileMenuBarButton").style.backgroundColor = "rgba(198, 119, 70, 0.5)";
         menuActive = true;
     }
     else
     {
         document.getElementById("mobileMenuBar").style.display = "none";
+        document.getElementById("mobileMenuBarButton").style.backgroundColor = "rgba(198, 119, 70, 0)";
         menuActive = false;
     }
 }
