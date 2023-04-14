7e9d715574111af5b7f8e7d62cba1802469a227e
brausjonas <brausjonas@gmail.com>
2023-04-14 23:54:41 +0200

    Build Shop
    Shop now shows the current coins of the player

--- FlufflParty/Assets/Scripts/ItemShop.cs
+++ FlufflParty/Assets/Scripts/ItemShop.cs
@@ -8,17 +8,19 @@ public class ItemShop : MonoBehaviour
     [SerializeField] private UIItem[] items;
 
     public static Player currentPlayer = null;
+    [SerializeField] private TMP_Text textPlayerCoins;
 
     public void Init(Player _currentPlayer)
     {
         currentPlayer = _currentPlayer;
+        textPlayerCoins.text = currentPlayer.coins + "";
     }
 
     private void Start()
     {
         foreach(UIItem ui in items)
         {
-            TMP_Text text = ui.GetComponentInChildren<TMP_Text>();
+            TMP_Text text = ui.GetComponentsInChildren<TMP_Text>()[0];
             text.text = ui.item.Cost + "";
         }
     }
