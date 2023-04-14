7f18be34a5bb52709afd769f4319f286bffb5787
brausjonas <brausjonas@gmail.com>
2023-04-15 00:36:39 +0200

    Automatic Password Fill for android Build (Script)

---
+++ FlufflParty/Assets/Scripts/AutoSetAndroidPassword.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+
+[InitializeOnLoad]
+public class AutoSetAndroidPassword
+{
+    static AutoSetAndroidPassword()
+    {
+        PlayerSettings.keystorePass = "ACD1576BbBACD";
+        PlayerSettings.keyaliasPass = "ACD1576BbBACD";
+    }
+}
